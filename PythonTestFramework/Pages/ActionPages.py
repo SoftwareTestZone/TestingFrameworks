b54c964137672586b6739b656ffe1325241e05f0
“SoftwareTestZone” <softwarelyzone@gmail.com>
2023-02-18 14:49:46 +0530

    WebDriver is opening

--- PythonTestFramework/Pages/ActionPages.py
+++ PythonTestFramework/Pages/ActionPages.py
@@ -1,2 +1,9 @@
-import selenium
-from selenium.webdriver import Chrome
+from selenium import webdriver
+
+# /Users/shorya/.jenkins/secrets/initialAdminPassword
+# b7d899b121f64f6db8b6dc228920a1b7 - Jenkins Passcode
+# This may also be found at: /Users/shorya/.jenkins/secrets/initialAdminPassword
+
+driver = webdriver.Chrome()
+
+driver.get('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
